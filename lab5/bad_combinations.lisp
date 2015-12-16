bb131c31096d1fc5d169e1e5df74e4e6b80cca1e
Igor Schelkonogov <ingver.yum@gmail.com>
2015-12-16 23:52:21 +0500

    Added a check

--- lab5/bad_combinations.lisp
+++ lab5/bad_combinations.lisp
@@ -1,8 +1,8 @@
 (defun combinations (n ls)
   (if (= n 1)
-      (if (null (cdr ls))
-          (list (list (car ls)))
-          (cons (list (car ls)) (combinations 1 (cdr ls))))
+      (cond ((null ls) nil)
+            ((null (cdr ls)) (list (list (car ls))))
+            (t (cons (list (car ls)) (combinations n (cdr ls)))))
       (if (null (cdr ls))
           nil
           (append (ins (car ls) (combinations (1- n) (cdr ls))) (combinations n (cdr ls))))))
@@ -14,4 +14,5 @@
 
 ;(print (ins 3 '((1 2) (4 5) (6 7))))
 (print (combinations 4 '(1 2 3 4)))
+(print (combinations 1 '()))
 
